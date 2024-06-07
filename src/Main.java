90ec9510d779f14146c9441f4c8cf70bb5e695a2
amenbruhh <pachison.artyom@yandex.ru>
2024-06-07 22:17:07 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // задача 1
+
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
+
